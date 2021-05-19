7e40bb10b96051257af3629fb3fb7826364573b1
miniact <gmangesh29@gmail.com>
2021-05-19 22:23:14 +0530

    heroku baseUrl

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,6 @@
 
 let AWSbaseURL;
 
-export default AWSbaseURL = "http://ec2-34-229-73-60.compute-1.amazonaws.com:8000"
+// export default AWSbaseURL = "http://ec2-34-229-73-60.compute-1.amazonaws.com:8000"
+
+export default AWSbaseURL = "https://djangonlpwhatsapp.herokuapp.com"
